f5afcd988462cebc598c7dc2cb3d91b95de2a5bd
Anton Konovalov <antoniuskonovalov@gmail.com>
2022-05-04 14:49:36 +0300

    drg

---
+++ public/JS_Vanilla_FinalProject.md
@@ -0,0 +1,3 @@
+Product store.
+Registration Form on the main page (must include full name and confirm password), user information appear in header section using LocalStorage for saving the user info.
+Adding products by "add product" button. Editing products by pen button.
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const path = require('path')
+const app = express()
+const userRouter = require('./routes/users')
+const PORT = process.env.PORT || 4000
+
+
+    app.get("/", (req, res) => {
+        console.log('Hello I am starting to work server.js')
+        // res.send('greetings')
+        // res.sendStatus(500)
+        // res.status(500).send('You did something wrong cabrone')
+        // res.status(500).json({ message: "Error"})
+        // res.json({ message: "Error"})
+        // res.download("server.js") 
+        // res.render("index")
+    })
+    app.use(express.static(path.join(__dirname, 'public')))
+    app.use(express.static('public'))
+    app.use('/users', userRouter)
+    app.listen(PORT, () => console.log(`Listening on port ${ PORT }`))
